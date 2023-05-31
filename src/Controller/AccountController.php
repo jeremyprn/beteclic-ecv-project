3c0d58d36b31bdeb5b9f4a99c48febe7b6c4be33
Nathan Dubocage <nathandubocage@gmail.com>
2023-05-31 22:16:53 +0200

    Add test on account template to display bet and event

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,12 +2,22 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Bet; 
 
 class AccountController extends AbstractController
 {
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+
     #[Route('/account', name: 'beteclic_account')]
     public function index(): Response
     {
@@ -17,9 +27,14 @@ class AccountController extends AbstractController
             $this->addFlash('danger', 'Vous devez être connecté pour accéder à votre compte.');
             return $this->redirectToRoute('app_login');
         }
-    
+
+        $bets = $this->entityManager
+        ->getRepository(Bet::class)
+        ->findAll();
+
         return $this->render('account/index.html.twig', [
-            'user' => $user
+            'user' => $user,
+            'bets' => $bets
         ]);
     }
 }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Bet;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +11,20 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'beteclic_home')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
-        return $this->render('home/index.html.twig');
+        // Un exemple d'appel à la base de données
+        // Si on veut faire des requêtes personnalisées, c'est possible, dans le repository
+        $bets = $entityManager->getRepository(Bet::class)->findAll();
+
+        if (!$bets) {
+            throw $this->createNotFoundException(
+                'No bets found'
+            );
+        }
+        
+        return $this->render('home/index.html.twig', [
+            'bets' => $bets
+        ]);
     }
 }
