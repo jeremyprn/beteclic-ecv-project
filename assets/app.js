d887933d63b8c9ff7cf94c9cdd31c7f7c54ec230
Nathan Dubocage <nathandubocage@gmail.com>
2023-06-02 10:19:10 +0200

    Add burger toggle

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,9 @@
 import "./styles/entry.scss";
+
+if (document.querySelector(".header") && window.innerWidth < 768) {
+  let burgerSection = document.querySelector(".header_wrapper_burger");
+  burgerSection.addEventListener("click", function () {
+    burgerSection.querySelector(".header_wrapper_burger_content").classList.toggle("active");
+    document.querySelector(".header_wrapper_nav").classList.toggle("-open");
+  });
+}
